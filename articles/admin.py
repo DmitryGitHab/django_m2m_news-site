a029f207431e7bef3c39d9639c7dffdea4b7e86b
Dmitry <babanov91@gmail.com>
2022-05-22 15:41:48 +0500

    add m2m relationship

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, ArticleTag, Tag
+
+
+class ArticleTagInline(admin.TabularInline):
+    model = ArticleTag
+    # formset = ArticleTagInLineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'published_at', 'id')
+    inlines = [ArticleTagInline]
+    # pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
+    inlines = [ArticleTagInline]
+    # pass
+
+
+# @admin.site.register(Article)
+# class ArticleAdmin(admin.ModelAdmin):
+#     inlines = (ArticleTagInline,)
+#     pass
+#
+#
+# @admin.site.register(Tag)
+# class TagAdmin(admin.ModelAdmin):
+#     inlines = (ArticleTagInline,)
+#     pass
--- articles/models.py
+++ articles/models.py
@@ -7,6 +7,7 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tag = models.ManyToManyField('Tag', verbose_name='Тематики статьи', through='ArticleTag')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +15,27 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название тэга')
+    # article = models.ManyToManyField(Article, through='ArticleTag')
+
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleTag(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, )
+    is_main = models.BooleanField(default='False', verbose_name='Основной')
+
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
