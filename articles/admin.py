04b7ffa97b6daa82ef78b2f434168da20089fd0d
Dmitry <babanov91@gmail.com>
2022-05-23 22:36:41 +0500

    only 1 main tag

--- articles/admin.py
+++ articles/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 from django.forms import BaseInlineFormSet
 
 from .models import Article, ArticleTag, Tag
 
 
+class ArticleTagInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main = False
+        for form in self.forms:
+            if form.cleaned_data.get("is_main"):
+                if not is_main:
+                    is_main = True
+                else:
+                    raise ValidationError('Главной может быть только одна тема')
+            if not is_main:
+                raise ValidationError('Выберите главный раздел')
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
 class ArticleTagInline(admin.TabularInline):
     model = ArticleTag
-    # formset = ArticleTagInLineFormset
+    formset = ArticleTagInlineFormset
     extra = 1
 
 
@@ -14,23 +35,9 @@ class ArticleTagInline(admin.TabularInline):
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'published_at', 'id')
     inlines = [ArticleTagInline]
-    # pass
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ['name', 'id']
     inlines = [ArticleTagInline]
-    # pass
-
-
-# @admin.site.register(Article)
-# class ArticleAdmin(admin.ModelAdmin):
-#     inlines = (ArticleTagInline,)
-#     pass
-#
-#
-# @admin.site.register(Tag)
-# class TagAdmin(admin.ModelAdmin):
-#     inlines = (ArticleTagInline,)
-#     pass
