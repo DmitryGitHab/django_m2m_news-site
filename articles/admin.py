b2e643c2a5040db8c0b3b3d7088677afc2a5a052
Dmitry <babanov91@gmail.com>
2022-05-28 11:47:14 +0500

    fix Is_Main trouble

--- articles/admin.py
+++ articles/admin.py
@@ -24,23 +24,6 @@ class ArticleTagInlineFormset(BaseInlineFormSet):
 
         return super().clean()  # вызываем базовый код переопределяемого метода
 
-# class ArticleTagInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         is_main = 0
-#         for form in self.forms:
-#             if form.cleaned_data.get("is_main"):
-#                 is_main += 1
-#             if is_main > 1:
-#                 raise ValidationError('Главной может быть только одна тема')
-#             if is_main == 0:
-#                 raise ValidationError('Укажите главный раздел')
-#             # В form.cleaned_data будет словарь с данными
-#             # каждой отдельной формы, которые вы можете проверить
-#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
-#             # таким образом объект не будет сохранен,
-#             # а пользователю выведется соответствующее сообщение об ошибке
-#
-#         return super().clean()  # вызываем базовый код переопределяемого метода
 
 class ArticleTagInline(admin.TabularInline):
     model = ArticleTag
